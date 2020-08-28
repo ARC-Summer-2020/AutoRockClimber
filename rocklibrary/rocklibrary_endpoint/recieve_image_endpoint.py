c15954388b452ce82f532e22a995b93fa8f3f1ae
mary <adams222@uwm.edu>
2020-08-27 21:42:02 -0500

    idky the files from this branch are changing when I change the rockLibrary enf point file?

--- rocklibrary/rocklibrary_endpoint/recieve_image_endpoint.py
+++ rocklibrary/rocklibrary_endpoint/recieve_image_endpoint.py
@@ -15,6 +15,8 @@ header = ['content-type']
 # Keys: imagePath, color
 imageUpload = {}
 
+colors = ['green', 'blue', 'yellow', 'red', 'purple', 'pink', 'black', 'brown', 'dark red', 'dark yellow', 'dark blue',
+    'light red', 'light green', 'grey', 'orange', 'white']
 @route('/rockLibrary/imageupload', method='POST')
 def getImageUploadAndColor():
     """Recieves request of image to be uploaded along with the
@@ -34,21 +36,26 @@ def getImageUploadAndColor():
         if headers.get(head) not in request.get_header(head):
             abort(406, 'Incorrect headers')
     
-    colors = ['green', 'blue', 'yellow', 'red', 'purple', 'pink', 'black', 'brown', 'dark red', 'dark yellow', 'dark blue',
-    'light red', 'light green', 'grey', 'orange', 'white']
+    
     colorRequest = request.forms.get('color')
     imageRequest = request.files.get('image')
-    userHeightRequest = request.forms.get('userHeight')
-    wallHeightRequest = request.forms.get('wallHeight')
-    
     if not (colorRequest and imageRequest):
-        abort(400, 'Bad request')
+            abort(400, 'Bad request')
 
     if not colorRequest in colors:
         abort(407, 'Bad color input')
-        
-    if not (userHeightRequest.isnumeric() and wallHeightRequest.isnumeric()):
-        abort(400, 'Bad number input')
+
+    userHeightRequest = request.forms.get('userHeight')
+    if userHeightRequest is not None and not userHeightRequest.isnumeric():
+        abort(400, 'Bad user height number input')
+    else:
+        userHeightRequest = ""
+
+    wallHeightRequest = request.forms.get('wallHeight')
+    if wallHeightRequest is not None and not wallHeightRequest.isnumeric():
+        abort(400, 'Bad user height number input')
+    else:
+        wallHeightRequest = ""
 
     imageName = imageRequest.filename.split('.')
     if len(imageName) > 2 or imageName[len(imageName)-1] not in ('jpeg', 'jpg', 'png'):
