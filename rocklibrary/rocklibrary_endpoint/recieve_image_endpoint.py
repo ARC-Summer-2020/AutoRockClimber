db0af9c226b1741dcbae477e393e4a8ac963f9d0
mary <adams222@uwm.edu>
2020-08-27 21:00:31 -0500

    Made changes to userHeight and wallHeight optional key inputs

--- rocklibrary/rocklibrary_endpoint/recieve_image_endpoint.py
+++ rocklibrary/rocklibrary_endpoint/recieve_image_endpoint.py
@@ -15,6 +15,8 @@ header = ['content-type']
 # Keys: imagePath, color
 imageUpload = {}
 
+colors = ['green', 'blue', 'yellow', 'red', 'purple', 'pink', 'black', 'brown', 'dark red', 'dark yellow', 'dark blue',
+    'light red', 'light green', 'grey', 'orange', 'white']
 @route('/rockLibrary/imageupload', method='POST')
 def getImageUploadAndColor():
     """Recieves request of image to be uploaded along with the
@@ -34,21 +36,31 @@ def getImageUploadAndColor():
         if headers.get(head) not in request.get_header(head):
             abort(406, 'Incorrect headers')
     
-    colors = ['green', 'blue', 'yellow', 'red', 'purple', 'pink', 'black', 'brown', 'dark red', 'dark yellow', 'dark blue',
-    'light red', 'light green', 'grey', 'orange', 'white']
+    
     colorRequest = request.forms.get('color')
     imageRequest = request.files.get('image')
+
     userHeightRequest = request.forms.get('userHeight')
+    if userHeightRequest is not None:
+        if not userHeightRequest.isnumeric():
+            abort(400, 'Bad user height number input')
+    else:
+        userHeightRequest = ""
+
     wallHeightRequest = request.forms.get('wallHeight')
-    
+    if wallHeightRequest is not None:
+            if not wallHeightRequest.isnumeric():
+                abort(400, 'Bad user height number input')
+    else:
+        wallHeightRequest = ""
+
     if not (colorRequest and imageRequest):
         abort(400, 'Bad request')
 
     if not colorRequest in colors:
         abort(407, 'Bad color input')
         
-    if not (userHeightRequest.isnumeric() and wallHeightRequest.isnumeric()):
-        abort(400, 'Bad number input')
+    
 
     imageName = imageRequest.filename.split('.')
     if len(imageName) > 2 or imageName[len(imageName)-1] not in ('jpeg', 'jpg', 'png'):
