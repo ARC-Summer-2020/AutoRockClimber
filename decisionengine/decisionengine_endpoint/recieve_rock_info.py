a950ba18259972e4c378e35c3cd5c9cf7cffd19e
Mars-98 <36772883+Mars-98@users.noreply.github.com>
2020-09-03 06:52:56 -0500

    decisionEngine endpoints (#16)
    * created dict to keep track of key/values, made wallHeight and userHeight endpoint request, fixed route path
    
    * fixed based on comments
    
    * forgot to delete a line
    
    * edits based on comments
    
    * made edits for steven

--- decisionengine/decisionengine_endpoint/recieve_rock_info.py
+++ decisionengine/decisionengine_endpoint/recieve_rock_info.py
@@ -12,7 +12,7 @@ header = ['content-type']
 # Keys of each element: placement, type
 rocks = []
 
-@route('decisionEngine/shortestPath', method='POST')
+@route('/decisionEngine/shortestPath', method='POST')
 def find_path():
     """Gets the request to find the shortest path on the given path.
     Example request: 
@@ -40,13 +40,33 @@ def find_path():
         if request.get_header(head) != headers.get(head):
             abort(406, 'Incorrect headers')
     rocks = request.json.get('rocks')
-    print(rocks)
     if not rocks:
         abort(400, 'Bad request')
     for rock in rocks:
         if not (rock.get('placement') or rock.get('type')):
             abort(400, 'Bad request')
+    userHeightError = "Bad user height number input"
+    wallHeightError = "Bad wall height number input"
+    userHeightRequest = var_request('userHeight', userHeightError, request)
+    wallHeightRequest = var_request('wallHeight', userHeightRequest, request)
 
-    return {'Message': 'Request successfully recieved, congratz on your rockz', 'Response rocks': rocks}
+    params = {}
+    add_KV_pair(params, 'rocks', rocks)
+    add_KV_pair(params, 'userHeight', userHeightRequest)
+    add_KV_pair(params, 'wallHeight', wallHeightRequest)
+
+    return {'response': params, 'Message': 'Request successfully recieved, congratz on your rockz'}
+    
+def add_KV_pair(dict, key, value):
+    if value is not '':
+        dict[key] = value
+
+def var_request(param, errorMsg, request):
+    varRequest = request.json.get(param)
+    if varRequest is None:
+        varRequest = '' 
+    elif not isinstance(varRequest, (int, float)):
+        abort(400, errorMsg)
+    return varRequest
 
 run(host='localhost', port=8001, reloader=True, debug=True)
